9a42688a3616e96b16e44f6e617235568775d3c3
Ae Studio <tech@ae-studio.ru>
2015-08-19 16:33:14 +0300

    Contact change

--- contact/index.php
+++ contact/index.php
@@ -2,7 +2,7 @@
     $APPLICATION->SetTitle("Контакты");
 ?>
     <div class="wrapper wrapper--fill clearfix">
-        <div class="breadcrumbs col-xs-7"><a href="/" class="breadcrumbs__item">главная</a> <span class="breadcrumbs__separator">&frasl;</span> <span class="breadcrumbs__current">Контакты</span></div>
+        <div class="breadcrumbs col-xs-7"><a href="/" class="breadcrumbs__item">главная</a> <span class="breadcrumbs__separator">⁄</span> <span class="breadcrumbs__current">Контакты</span></div>
         <h1 class="page-title page-title--h1 col-xs-9">
             Контакты
         </h1>
@@ -10,14 +10,14 @@
     <div class="wrapper wrapper--fill content-block clearfix">
         <div class="col-xs-10 centered-col clearfix about-page">
             <div class="content-block__img-title">
-                <iframe src="https://www.google.com/maps/d/embed?mid=zkLLq1bJ-4ZI.kxNpvmqoEfF0" width="780" height="300"></iframe>
+                <iframe src="https://www.google.com/maps/d/u/0/embed?mid=zkLLq1bJ-4ZI.kxNpvmqoEfF0" width="780" height="300"></iframe>
             </div>
             <div class="col-xs-3 left">
                 <p class="content-block__sect-title">Местонахождение:</p>
                 <div class="content-block__text">
                     Украина, Одесская область,<br>
                     г. Белгород-Днестровский,<br>
-                    ул. Саночная, 14А и<br>
+                    ул. Солнечная, 14А и<br>
                     ул. Победы, 9
                 </div>
             </div>
